eb2e9bd31a3f9cc5be2eea873f9bc2ea045e21ac
Ben Fullenkamp <benfullenkamp8@gmail.com>
2024-10-24 21:29:01 -0400

    Submitted inclass 1

--- Module 1/inclass_exercise1.py
+++ Module 1/inclass_exercise1.py
@@ -1,112 +1,121 @@
 # Benjamin Fullenkamp
+# CSC6013
+# In-class Exericse 1
 
 
 class Node:
     def __init__(self, d):
-        self.data = d
-        self.next = None
+        self.Data = d
+        self.Next = None
 
 
 class LinkedList:
     def __init__(self, d=None):
         if d == None:
-            self.header = None
-            self.current = None
+            self.Header = None
+            self.Current = None
         else:
-            self.header = Node(d)
-            self.current = self.header
+            self.Header = Node(d)
+            self.Current = self.Header
+
+    def nextCurrent(self):
+        """Change the Current to the next Node in the LinkedList"""
+
+        if self.Current.Next is not None:
+            self.Current = self.Current.Next
+        else:
+            self.Current = self.Header
+
+    def resetCurrent(self):
+        """Set the Current node to Header Node"""
+
+        self.Current = self.Header
+
+    def getCurrent(self):
+        """Return the Data of the Current Node"""
+
+        if self.Current is None:
+            return None
+        else:
+            return self.Current.Data
 
     def insertBeginning(self, d):
         """Insert a node at the beginning of LinkedList"""
 
-        if self.header is None:
-            self.header = Node(d)
-            self.current = self.header
+        if self.Header is None:
+            self.Header = Node(d)
+            self.Current = self.Header
         else:
             tmp = Node(d)
-            tmp.next = self.header
-            self.header = tmp
+            tmp.Next = self.Header
+            self.Header = tmp
 
     def insertCurrentNext(self, d):
         """Insert a Node next to the Current Node"""
 
-        if self.header is None:
-            self.header = Node(d)
-            self.current = self.header
+        if self.Header is None:
+            self.Header = Node(d)
+            self.Current = self.Header
         else:
             tmp = Node(d)
-            tmp.next = self.current.next
-            self.current.next = tmp
+            tmp.Next = self.Current.Next
+            self.Current.Next = tmp
 
     def removeCurrentNext(self):
         """Remove the Node that the Current Node points to"""
 
-        if self.current.next is None:
+        if self.Current.Next is None:
             return None
         else:
-            ans = self.current.next.data
-            self.current.next = self.current.next.next
+            ans = self.Current.Next.Data
+            self.Current.Next = self.Current.Next.Next
             return ans
 
-    def nextCurrent(self):
-        """Change the Current node to Node that the Current points to"""
-
-        if self.current.next is not None:
-            self.current = self.current.next
-        else:
-            self.current = self.header
-
-    def resetCurrent(self):
-        """Set the current node to first Node in the LinkedList"""
-
-        self.current = self.header
-
-    def getCurrent(self):
-        """Return the data of the Current Node"""
-
-        if self.current is None:
-            return None
-        else:
-            return self.current.data
-
     def printList(self, msg="====="):
-        """Print the data of each data and what the Current Node is"""
+        """Print the Data of each Node and what the Current Node is"""
 
-        p = self.header
+        p = self.Header
         print("====", msg)
 
         while p is not None:
-            print(p.data, end=" ")
-            p = p.next
+            print(p.Data, end=" ")
+            p = p.Next
 
-        if self.current is not None:
-            print(f"Current: {self.current.data}")
+        if self.Current is not None:
+            print(f"Current: {self.Current.Data}")
         else:
             print("Empty Linked List")
         print("------------------------------------")
 
 
-if __name__ == "__main__":
+def main():
+    # Create an empty LinkedList
     mylist = LinkedList()
-    mylist.printList("List Created")
+
+    # Insert the 6 Integers to the List
     mylist.insertBeginning(91)
     mylist.insertBeginning(56)
     mylist.insertBeginning(34)
     mylist.insertBeginning(11)
     mylist.insertBeginning(88)
     mylist.insertBeginning(76)
+
+    # Print out the Current status of the list
     mylist.printList("Adding 6 Integers to List")
 
+    # Push the Current to the third element of the list
     mylist.resetCurrent()
-    mylist.printList("Resetting the Current")
-
     mylist.nextCurrent()
     mylist.nextCurrent()
-    mylist.printList(f"Setting current to the third element in the list")
-    print(f"The current is: {mylist.getCurrent()}")
 
+    # Remove the Next to the Current element
     mylist.removeCurrentNext()
-    mylist.printList(f"Removing next the Current element")
 
+    # Insert 23 next to the Current element
     mylist.insertCurrentNext(23)
-    mylist.printList(f"Inserting 23 next the Current element")
+
+    # Print the Current status of the list
+    mylist.printList(f"Current status of list after all operations")
+
+
+main()
